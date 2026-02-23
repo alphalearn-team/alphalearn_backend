7e5cea0937dda09ab6c81bd700442d2415c2ded3
jiugeng <130973940+jiugengw@users.noreply.github.com>
2026-02-24 00:00:40 +0800

    chore: remove obsolete LessonConcept join entity and endpoints

--- src/main/java/com/example/demo/lessonconcept/LessonConceptRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.lessonconcept;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonConceptRepository extends JpaRepository<LessonConcept, LessonConceptId> {
-    long countByIdConceptId(Integer conceptId);
-}
